f2fa080bdb61eb1b90b07d96933e3acc592dd761
cherry <i@cherry.style>
2021-07-21 23:25:35 +0900

    docs(styled): set up storybook docs

---
+++ packages/styled/.storybook/preview.js
@@ -0,0 +1,13 @@
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  viewMode: 'docs',
+  previewTabs: {
+    'storybook/docs/panel': { index: -1 },
+  },
+};
